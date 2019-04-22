9b1af3fb00084fc45aa910854d3c597bb070d021
K0240017 <K0240017@test.htsc.com.cn>
2019-04-22 17:21:35 +0800

    first commit 创建绘制圆环图表的view

---
+++ XXHPieChartView/XXHPieChartView/XXHPieChartView.h
@@ -0,0 +1,17 @@
+//
+//  XXHPieChartView.h
+//  XXHPieChartView
+//
+//  Created by ZL-ios on 2019/4/22.
+//  Copyright © 2019年 XXH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XXHPieChartView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XXHPieChartView/XXHPieChartView/XXHPieChartView.m
@@ -0,0 +1,21 @@
+//
+//  XXHPieChartView.m
+//  XXHPieChartView
+//
+//  Created by ZL-ios on 2019/4/22.
+//  Copyright © 2019年 XXH. All rights reserved.
+//
+
+#import "XXHPieChartView.h"
+
+@implementation XXHPieChartView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
